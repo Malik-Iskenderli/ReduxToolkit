a75657836495cb057c58bc8256882f88025775d1
ElchinRustamov <elchin.rustamov@yahoo.com>
2023-10-03 22:49:07 +0400

    get api redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increment } from './redux/app/features/counter/counterSlice';
 import { checkedDecrement, checkedIncrement } from './redux/app/features/theme/themeSpanSlice';
+import { getCountry } from './redux/app/features/countApi/countrySlice';
 
 function App() {
+  const dispatch = useDispatch()
 const {counter} = useSelector(state => state.counter)
-
-const dispatch = useDispatch()
+const {country} = useSelector(state => state.country)
+useEffect(()=>{
+  dispatch(getCountry())
+},[])
+console.log("country",country)  
   return (
     <div className="App">
         <span onClick={()=> dispatch(decrement())} 
--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit"
 import counterSlice from "./features/counter/counterSlice"
 import themeSpanSlice from "./features/theme/themeSpanSlice"
+import countryReducer from "./features/countApi/countrySlice"
 
 
 export const store = configureStore({
 reducer:{
     counter : counterSlice,
     themeSpan : themeSpanSlice,
+    country : countryReducer,
 }
 })
